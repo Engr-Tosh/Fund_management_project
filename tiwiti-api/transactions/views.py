618a649a3f1920a40fc72dbf965bfe0e2a86dc7b
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-03-29 14:59:12 +0100

    Added views for the transactions app

--- tiwiti-api/transactions/views.py
+++ tiwiti-api/transactions/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import response, generics, status
+from .serializers import (
+    DepositSerializer,
+    WithdrawalSerializer,
+    BalanceSerializer,
+)
+from .models import (
+    Deposit,
+    Withdrawal,
+    Balance,
+)
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework import permissions
+from django.contrib.auth import authenticate
 
-# Create your views here.
+# Authenticated Users can make deposits
+class DepositView(generics.ListCreateAPIView):
+    """Users can make deposits"""
+    queryset = Deposit.objects.all()
+    serializer_class = DepositSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Authenticated Users can make withdrawals
+class WithdrawalView(generics.ListCreateAPIView):
+    """Users can place withdrawals"""
+    queryset = Withdrawal.objects.all()
+    serializer_class = WithdrawalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Authenticated Users can see their balance:
+class BalanceView(generics.RetrieveAPIView):
+    queryset = Balance.objects.all()
+    serializer_class = BalanceSerializer
+    permission_classes = [permissions.IsAuthenticated]
